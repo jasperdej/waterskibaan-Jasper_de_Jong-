5282ec50b0123de599d4cad6386750ecf27536fc
jdejong <tottie2014@gmail.com>
2019-09-17 11:53:50 +0200

    Add project files.

---
+++ Wpf_Waterskibaan_project/Sporter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Waterskibaan_project
+{
+    class Sporter : IMoves
+    {
+        public int Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
