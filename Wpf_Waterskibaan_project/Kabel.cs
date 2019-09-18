8e24082e67ada1681e48e2120e29729908801408
jdejong <tottie2014@gmail.com>
2019-09-18 08:26:10 +0200

    Klasse Kabel toegevoegd en deels uitgewerkt

---
+++ Wpf_Waterskibaan_project/Kabel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Waterskibaan_project
+{
+    class Kabel
+    {
+
+        private LinkedList<Lijn> _lijnen;
+
+        public Boolean IsStartPositieLeeg()
+        {
+            if(_lijnen[0] == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public void NeemLijnInGebruik(Lijn lijn)
+        {
+
+        }
+
+        public void VerschuifLijnen()
+        {
+
+        }
+
+        public Lijn VerwijderLijnVanKabel()
+        {
+
+        }
+
+        public string ToString()
+        {
+
+        }
+    }
+}
